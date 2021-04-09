42949d10d07716d75d2e7ecf3d5cff19d94e43e2
alienatedsec <s7aw3k@gmail.com>
2021-04-09 12:47:01 +0100

    [Changes] Align to the latest 	modified:   src/static/static/home/yi-hack-v5/etc/mqttv4.conf 	new file:   src/static/static/home/yi-hack-v5/script/check_conf.sh 	modified:   src/www/httpd/cgi-bin/fw_upgrade.sh

---
+++ src/static/static/home/yi-hack-v5/script/check_conf.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+
+SYSTEM_CONF_FILE="/tmp/sd/yi-hack-v5/etc/system.conf"
+CAMERA_CONF_FILE="/tmp/sd/yi-hack-v5/etc/camera.conf"
+MQTTV4_CONF_FILE="/tmp/sd/yi-hack-v5/etc/mqttv4.conf"
+
+PARMS1="
+HTTPD=yes
+TELNETD=yes
+SSHD=yes
+FTPD=yes
+BUSYBOX_FTPD=no
+DISABLE_CLOUD=no
+REC_WITHOUT_CLOUD=no
+MQTT=no
+RTSP=yes
+RTSP_STREAM=high
+RTSP_AUDIO=no
+ONVIF=yes
+ONVIF_WSDD=yes
+ONVIF_PROFILE=high
+ONVIF_NETIF=wlan0
+ONVIF_WM_SNAPSHOT=no
+NTPD=yes
+NTP_SERVER=pool.ntp.org
+PROXYCHAINSNG=no
+SWAP_FILE=no
+RTSP_PORT=554
+ONVIF_PORT=80
+HTTPD_PORT=8080
+USERNAME=
+PASSWORD=
+TIMEZONE=
+FREE_SPACE=0
+FTP_UPLOAD=no
+FTP_HOST=
+FTP_DIR=
+FTP_DIR_TREE=no
+FTP_USERNAME=
+FTP_PASSWORD=
+FTP_FILE_DELETE_AFTER_UPLOAD=yes
+SSH_PASSWORD=
+CRONTAB="
+
+PARMS2="
+SWITCH_ON=yes
+SAVE_VIDEO_ON_MOTION=yes
+SENSITIVITY=low
+AI_HUMAN_DETECTION=no
+SOUND_DETECTION=no
+SOUND_SENSITIVITY=80
+LED=no
+ROTATE=no
+IR=yes"
+
+PARMS3="
+MQTT_IP=0.0.0.0
+MQTT_PORT=1883
+MQTT_CLIENT_ID=yi-cam
+MQTT_USER=
+MQTT_PASSWORD=
+MQTT_PREFIX=yicam
+MQTT_PREFIX_CONFIG=
+TOPIC_BIRTH_WILL=status
+TOPIC_AI_HUMAN_DETECTION=ai_human_detection
+TOPIC_MOTION=motion_detection
+TOPIC_MOTION_IMAGE=motion_detection_image
+TOPIC_MOTION_FILES=motion_files
+TOPIC_SOUND_DETECTION=sound_detection
+BIRTH_MSG=online
+WILL_MSG=offline
+AI_HUMAN_DETECTION_START_MSG=human_start
+AI_HUMAN_DETECTION_STOP_MSG=human_stop
+MOTION_START_MSG=motion_start
+MOTION_STOP_MSG=motion_stop
+SOUND_DETECTION_MSG=sound_detected
+MQTT_KEEPALIVE=120
+MQTT_QOS=1
+MQTT_RETAIN_BIRTH_WILL=1
+MQTT_RETAIN_AI_HUMAN_DETECTION=1
+MQTT_RETAIN_MOTION=1
+MQTT_RETAIN_MOTION_IMAGE=1
+MQTT_RETAIN_MOTION_FILES=1
+MQTT_RETAIN_SOUND_DETECTION=1"
+
+for i in $PARMS1
+do
+    if [ ! -z "$i" ]; then
+        PAR=$(echo "$i" | cut -d= -f1)
+        MATCH=$(cat $SYSTEM_CONF_FILE | grep $PAR)
+        if [ -z "$MATCH" ]; then
+            echo "$i" >> $SYSTEM_CONF_FILE
+        fi
+    fi
+done
+
+for i in $PARMS2
+do
+    if [ ! -z "$i" ]; then
+        PAR=$(echo "$i" | cut -d= -f1)
+        MATCH=$(cat $CAMERA_CONF_FILE | grep $PAR)
+        if [ -z "$MATCH" ]; then
+            echo "$i" >> $CAMERA_CONF_FILE
+        fi
+    fi
+done
+
+for i in $PARMS3
+do
+    if [ ! -z "$i" ]; then
+        PAR=$(echo "$i" | cut -d= -f1)
+        MATCH=$(cat $MQTTV4_CONF_FILE | grep $PAR)
+        if [ -z "$MATCH" ]; then
+            echo "$i" >> $MQTTV4_CONF_FILE
+        fi
+    fi
+done
--- src/www/httpd/cgi-bin/fw_upgrade.sh
+++ src/www/httpd/cgi-bin/fw_upgrade.sh
@@ -2,8 +2,8 @@
 
 YI_HACK_PREFIX="/tmp/sd/yi-hack"
 
-export PATH=/usr/bin:/usr/sbin:/bin:/sbin:/home/base/tools:/home/app/localbin:/home/base:/tmp/sd/yi-hack/bin:/tmp/sd/yi-hack/sbin:/tmp/sd/yi-hack/usr/bin:/tmp/sd/yi-hack/usr/sbin
-export LD_LIBRARY_PATH=/lib:/usr/lib:/home/lib:/home/qigan/lib:/home/app/locallib:/tmp/sd:/tmp/sd/gdb:/tmp/sd/yi-hack/lib
+export PATH=/usr/bin:/usr/sbin:/bin:/sbin:/home/base/tools:/home/app/localbin:/home/base:/tmp/sd/yi-hack-v5/bin:/tmp/sd/yi-hack-v5/sbin:/tmp/sd/yi-hack-v5/usr/bin:/tmp/sd/yi-hack-v5/usr/sbin
+export LD_LIBRARY_PATH=/lib:/usr/lib:/home/lib:/home/qigan/lib:/home/app/locallib:/tmp/sd:/tmp/sd/gdb:/tmp/sd/yi-hack-v5/lib
 
 
 NAME="$(echo $QUERY_STRING | cut -d'=' -f1)"
@@ -16,8 +16,8 @@ fi
 if [ "$VAL" == "info" ] ; then
     printf "Content-type: application/json\r\n\r\n"
 
-    FW_VERSION=`cat /tmp/sd/yi-hack/version`
-    LATEST_FW=`/tmp/sd/yi-hack/usr/bin/wget -O -  https://api.github.com/repos/roleoroleo/yi-hack-Allwinner-v2/releases/latest 2>&1 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+    FW_VERSION=`cat /tmp/sd/yi-hack-v5/version`
+    LATEST_FW=`/usr/bin/wget -O -  https://api.github.com/repos/alienatedsec/yi-hack-v5/releases/latest 2>&1 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
 
     printf "{\n"
     printf "\"%s\":\"%s\",\n" "fw_version"      "$FW_VERSION"
@@ -50,19 +50,19 @@ elif [ "$VAL" == "upgrade" ] ; then
     cd /tmp/sd/.fw_upgrade
 
     MODEL_SUFFIX=`cat $YI_HACK_PREFIX/model_suffix`
-    FW_VERSION=`cat /tmp/sd/yi-hack/version`
+    FW_VERSION=`cat /tmp/sd/yi-hack-v5/version`
     if [ -f /tmp/sd/${MODEL_SUFFIX}_x.x.x.tgz ]; then
         mv /tmp/sd/${MODEL_SUFFIX}_x.x.x.tgz /tmp/sd/.fw_upgrade/${MODEL_SUFFIX}_x.x.x.tgz
         LATEST_FW="x.x.x"
     else
-        LATEST_FW=`/tmp/sd/yi-hack/usr/bin/wget -O -  https://api.github.com/repos/roleoroleo/yi-hack-Allwinner-v2/releases/latest 2>&1 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+        LATEST_FW=`/usr/bin/wget -O -  https://api.github.com/repos/alienatedsec/yi-hack-v5/releases/latest 2>&1 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
         if [ "$FW_VERSION" == "$LATEST_FW" ]; then
             printf "Content-type: text/html\r\n\r\n"
             printf "No new firmware available."
             exit
         fi
 
-        /tmp/sd/yi-hack/usr/bin/wget https://github.com/roleoroleo/yi-hack-Allwinner-v2/releases/download/$LATEST_FW/${MODEL_SUFFIX}_${LATEST_FW}.tgz
+        /usr/bin/wget https://api.github.com/repos/alienatedsec/yi-hack-v5/releases/download/$LATEST_FW/${MODEL_SUFFIX}_${LATEST_FW}.tgz
         if [ ! -f ${MODEL_SUFFIX}_${LATEST_FW}.tgz ]; then
             printf "Content-type: text/html\r\n\r\n"
             printf "Unable to download firmware file."
@@ -77,8 +77,8 @@ elif [ "$VAL" == "upgrade" ] ; then
     # Prepare new hack
     tar zxvf ${MODEL_SUFFIX}_${LATEST_FW}.tgz
     rm ${MODEL_SUFFIX}_${LATEST_FW}.tgz
-    mkdir -p /tmp/sd/.fw_upgrade/yi-hack/etc
-    cp -rf /tmp/sd/.fw_upgrade.conf/* /tmp/sd/.fw_upgrade/yi-hack/etc/
+    mkdir -p /tmp/sd/.fw_upgrade/yi-hack-v5/etc
+    cp -rf /tmp/sd/.fw_upgrade.conf/* /tmp/sd/.fw_upgrade/yi-hack-v5/etc/
 
     # Report the status to the caller
     printf "Content-type: text/html\r\n\r\n"
