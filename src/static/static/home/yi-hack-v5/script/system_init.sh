61e3495b580b65cc3d48ac868bfc375b033c8d9a
alienatedsec <s7aw3k@gmail.com>
2021-05-17 19:12:06 +0100

    [Changes] Small reverts post-historical testing 	modified:   src/static/static/home/yi-hack-v5/script/system_init.sh

--- src/static/static/home/yi-hack-v5/script/system_init.sh
+++ src/static/static/home/yi-hack-v5/script/system_init.sh
@@ -1,27 +1,21 @@
 #!/bin/sh
 
-#if [ -f "/tmp/sd/yi-hack-v5.7z" ] || [ -d "/tmp/sd/yi-hack-v5" ]; then
-YI_HACK_V5_PREFIX="/tmp/sd"
-YI_HACK_V5_PREFIX2="/home"
-YI_PREFIX="/home/app"
-YI_PREFIX2="/tmp/sd"
-UDHCPC_SCRIPT_DEST="/home/app/script/default.script"
-#elif [ -d "/usr/yi-hack-v5" ]; then
-#    YI_HACK_V5_PREFIX="/usr"
-#    YI_PREFIX="/home"
-#    UDHCPC_SCRIPT_DEST="/home/default.script"
-#elif [ -d "/home/yi-hack-v5" ]; then
-#    YI_HACK_V5_PREFIX="/home"
-#    YI_PREFIX="/home/app"
-#    UDHCPC_SCRIPT_DEST="/home/app/script/default.script"
-#fi
+if [ -d "/usr/yi-hack-v5" ]; then
+    YI_HACK_V5_PREFIX="/usr"
+    YI_PREFIX="/home"
+    UDHCPC_SCRIPT_DEST="/home/default.script"
+elif [ -d "/home/yi-hack-v5" ]; then
+    YI_HACK_V5_PREFIX="/home"
+    YI_PREFIX="/home/app"
+    UDHCPC_SCRIPT_DEST="/home/app/script/default.script"
+fi
 
-ARCHIVE_FILE="$YI_HACK_V5_PREFIX/yi-hack-v5.7z"
+ARCHIVE_FILE="$YI_HACK_V5_PREFIX/yi-hack-v5/yi-hack-v5.7z"
 DESTDIR="$YI_HACK_V5_PREFIX/yi-hack-v5"
 
 DHCP_SCRIPT_DEST="/home/app/script/wifidhcp.sh"
-UDHCP_SCRIPT="$YI_HACK_V5_PREFIX2/yi-hack-v5/script/default.script"
-DHCP_SCRIPT="$YI_HACK_V5_PREFIX2/yi-hack-v5/script/wifidhcp.sh"
+UDHCP_SCRIPT="$YI_HACK_V5_PREFIX/yi-hack-v5/script/default.script"
+DHCP_SCRIPT="$YI_HACK_V5_PREFIX/yi-hack-v5/script/wifidhcp.sh"
 
 files=`find $YI_PREFIX -maxdepth 1 -name "*.7z"`
 if [ ${#files[@]} -gt 0 ]; then
@@ -30,14 +24,13 @@ if [ ${#files[@]} -gt 0 ]; then
 fi
 
 if [ -f $ARCHIVE_FILE ]; then
-    mkdir -p $DESTDIR
 	/home/base/tools/7za x $ARCHIVE_FILE -y -o$DESTDIR
 	rm $ARCHIVE_FILE
 fi
 
 if [ ! -f $YI_PREFIX/cloudAPI_real ]; then
 	mv $YI_PREFIX/cloudAPI $YI_PREFIX/cloudAPI_real
-	cp $YI_HACK_V5_PREFIX2/yi-hack-v5/script/cloudAPI $YI_PREFIX/
+	cp $YI_HACK_V5_PREFIX/yi-hack-v5/script/cloudAPI $YI_PREFIX/
         rm $UDHCPC_SCRIPT_DEST
         cp $UDHCP_SCRIPT $UDHCPC_SCRIPT_DEST
 	if [ -f $DHCP_SCRIPT_DEST ]; then
@@ -46,8 +39,8 @@ if [ ! -f $YI_PREFIX/cloudAPI_real ]; then
 	fi
 fi
 
-mkdir -p $YI_HACK_V5_PREFIX2/yi-hack-v5/etc/crontabs
-mkdir -p $YI_HACK_V5_PREFIX2/yi-hack-v5/etc/dropbear
+mkdir -p $YI_HACK_V5_PREFIX/yi-hack-v5/etc/crontabs
+mkdir -p $YI_HACK_V5_PREFIX/yi-hack-v5/etc/dropbear
 
 # Comment out all the cloud stuff from base/init.sh
 sed -i '/^\.\/watch_process/s/^/#/' /home/app/init.sh
