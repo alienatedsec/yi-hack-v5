e075c5ee4760790ea2c8d3363d806d4704f7e954
Dawid Malinowski <dawidmalina@gmail.com>
2022-02-15 23:22:21 +0100

    Update script

--- src/static/static/home/yi-hack-v5/script/system.sh
+++ src/static/static/home/yi-hack-v5/script/system.sh
@@ -197,7 +197,7 @@ if [[ $(get_config RTSP) == "yes" ]] ; then
         HIGHWIDTH="1920"
         HIGHHEIGHT="1080"
     fi
-        rRTSPServer -r $RRTSP_RES -p $RRTSP_PORT &
+        rRTSPServer -r $RRTSP_RES -p $RRTSP_PORT -a $RRTSP_AUDIO &
     if [[ $(get_config RTSP_STREAM) == "low" ]]; then
         h264grabber -r low -m $MODEL_SUFFIX -f &
         ONVIF_PROFILE_1="--name Profile_1 --width 640 --height 360 --url rtsp://%s$D_RTSP_PORT/ch0_1.h264 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh?res=low$WATERMARK --type H264"
@@ -216,8 +216,7 @@ if [[ $(get_config RTSP) == "yes" ]] ; then
             ONVIF_PROFILE_0="--name Profile_0 --width $HIGHWIDTH --height $HIGHHEIGHT --url rtsp://%s$D_RTSP_PORT/ch0_0.h264 --snapurl http://%s$D_HTTPD_PORT/cgi-bin/snapshot.sh?res=high$WATERMARK --type H264"
         fi
     fi
-#Seems to be killing the resource
-    #$YI_HACK_PREFIX/script/wd_rtsp.sh &
+    $YI_HACK_PREFIX/script/wd_rtsp.sh &
 fi
 
 if [[ $MODEL_SUFFIX == "yi_dome_1080p" ]] || [[ $MODEL_SUFFIX == "yi_cloud_dome_1080p" ]] ; then
