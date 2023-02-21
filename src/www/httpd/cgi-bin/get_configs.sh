58c4392ab29f38563e95c14f781dd7a793f650d9
roleo <roleo@iperbole.bologna.it>
2023-02-21 11:10:53 +0100

    Add homever to camera config

--- src/www/httpd/cgi-bin/get_configs.sh
+++ src/www/httpd/cgi-bin/get_configs.sh
@@ -40,6 +40,11 @@ if [ "$CONF_TYPE" == "system" ] ; then
     printf "\"%s\":\"%s\",\n"  "HOSTNAME" "$(cat $YI_HACK_PREFIX/etc/hostname | sed -r 's/\\/\\\\/g;s/"/\\"/g;')"
 fi
 
+if [ "$CONF_TYPE" == "camera" ] ; then
+    HOMEVER=$(cat /home/homever)
+    printf "\"%s\":\"%s\",\n"  "HOMEVER" "$HOMEVER"
+fi
+
 # Empty values to "close" the json
 printf "\"%s\":\"%s\"\n"  "NULL" "NULL"
 
