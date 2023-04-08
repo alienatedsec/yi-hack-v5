8c62377cdfcc3c4261bbe286e022e531c3aafb75
alienatedsec <73167064+alienatedsec@users.noreply.github.com>
2023-04-08 23:39:34 +0100

    [Revert] An old save method

--- src/www/httpd/cgi-bin/set_configs.sh
+++ src/www/httpd/cgi-bin/set_configs.sh
@@ -2,6 +2,7 @@
 YI_HACK_PREFIX="/tmp/sd/yi-hack-v5"
 
 sedencode(){
+#  echo -e "$(sed 's/\\/\\\\\\/g;s/\&/\\\&/g;s/\//\\\//g;')"
   echo "$(sed 's/\\/\\\\/g;s/\&/\\\&/g;s/\//\\\//g;')"
 }
 removedoublequotes(){
@@ -12,7 +13,7 @@ get_conf_type()
 {
     CONF="$(echo $QUERY_STRING | cut -d'=' -f1)"
     VAL="$(echo $QUERY_STRING | cut -d'=' -f2)"
-    if [ $CONF = "conf" ] ; then
+    if [ $CONF == "conf" ] ; then
         echo $VAL
     fi
 }
@@ -20,7 +21,7 @@ get_conf_type()
 CONF_TYPE="$(get_conf_type)"
 CONF_FILE=""
 
-if [ "$CONF_TYPE" = "mqtt" ] ; then
+if [ "$CONF_TYPE" == "mqtt" ] ; then
     CONF_FILE="$YI_HACK_PREFIX/etc/mqttv4.conf"
 else
     CONF_FILE="$YI_HACK_PREFIX/etc/$CONF_TYPE.conf"
@@ -28,18 +29,10 @@ fi
 
 read -r POST_DATA
 ROWS=$(echo "$POST_DATA" | jq -r '. | keys[] as $k | "\($k)=\(.[$k])"')
-
-# Create a temporary file to store the modified configuration file
-TMP_CONF_FILE=$(mktemp)
-
-# Copy the original configuration file to the temporary file
-cp $CONF_FILE $TMP_CONF_FILE
-
-# Loop through each key-value pair and update the temporary configuration file
 for ROW in $ROWS; do
     KEY=$(echo $ROW | cut -d'=' -f1)
     VALUE=$(echo $ROW | cut -d'=' -f2)
-    if [ "$KEY" = "HOSTNAME" ] ; then
+    if [ "$KEY" == "HOSTNAME" ] ; then
         if [ -z $VALUE ] ; then
             # Use 2 last MAC address numbers to set a different hostname
             MAC=$(cat /sys/class/net/wlan0/address|cut -d ':' -f 5,6|sed 's/://g')
@@ -54,21 +47,17 @@ for ROW in $ROWS; do
             echo "$VALUE" > $YI_HACK_PREFIX/etc/hostname
         fi
     else
-        if [ "$KEY" = "TIMEZONE" ] ; then
+        if [ "$KEY" == "TIMEZONE" ] ; then
             echo $VALUE > /etc/TZ
         fi
         VALUE=$(echo "$VALUE" | sedencode)
-        sed -i "s/^\(${KEY}[[:blank:]]*=[[:blank:]]*\).*$/\1${VALUE}/" $TMP_CONF_FILE
+        sed -i "s/^\(${KEY}[[:blank:]]*=[[:blank:]]*\).*$/\1${VALUE}/" $CONF_FILE
     fi
+    
 done
 
-# Copy the modified temporary file back to the original configuration file
-cp $TMP_CONF_FILE $CONF_FILE
+# Yeah, it's pretty ugly.
 
-# Remove the temporary file
-rm $TMP_CONF_FILE
-
-# Send a JSON response back to the client with an error flag set to `false`
 printf "Content-type: application/json\r\n\r\n"
 printf "{\n"
 printf "\"%s\":\"%s\"\\n" "error" "false"
